ca790108e146085622331c656b9ac6e743146ed9
Andriy Danilyuk <andy.danilyuk@gmail.com>
2019-09-30 12:23:22 +0300

    Orders, delete checked

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/product.model';
 import { DataService } from '../core/data.service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-orders',
@@ -11,51 +11,72 @@ import { Observable, Subject } from 'rxjs';
 })
 export class OrdersComponent implements OnInit {
 
-  observableProduct: Observable<Product[]>;  
+  products$: Observable<Product[]>;  
   products: Product[] = [];
   product: Product;
   errorMessage: string;
-  ordersChanged = new Subject<Product[]>();
   ordersChecked: any[] = [];
+  ordersChanged$ = new Subject<Product[]>();
+
+  i: number = 0;
+  
 
   constructor(
-    private dataServive: DataService,
-    private route: ActivatedRoute
+    private dataServive: DataService
   ) { }
 
   ngOnInit() {
-    this.observableProduct = this.dataServive.fetchOrders();
-    this.observableProduct.subscribe(
+    this.products$ = this.dataServive.fetchOrders();
+    this.products$.subscribe(
       products => this.products = products,
       error => this.errorMessage = <any>error
     );
+
+    this.ordersChanged$.subscribe(
+      orders => this.ordersChecked = orders,
+      error => this.errorMessage = error
+    );
   }
 
-  checkedOrder(id: number, event: Event) {
-    if (event) {
-      this.ordersChecked.push({id: id});
+  // checkedOrder(id: number, event: Event) {
+  //   if (event) {
+  //     this.ordersChecked.push({id: id});
+  //     this.ordersChanged$.next(this.ordersChecked.slice());
+  //   } else {
+  //     const index = this.ordersChecked.find(
+  //       list => list.id === id
+  //     );
+  //     this.ordersChecked.splice(index, 1);
+  //     this.ordersChanged$.next(this.ordersChecked.slice());
+  //   }    
+  //   console.log(this.ordersChecked);
+  //   console.log(this.ordersChanged$);
+  // }
+  checkedOrder(product: Product) {
+    if (this.ordersChecked.find(x => x == product)) {
+      this.ordersChecked.splice(this.ordersChecked.indexOf(product), 1);
+      this.ordersChanged$.next(this.ordersChecked.slice());
     } else {
-      const index = this.ordersChecked.find(
-        list => list.id === id
-      );
-      this.ordersChecked.splice(index, 1);
-    }    
+      this.ordersChecked.push(product);
+      this.ordersChanged$.next(this.ordersChecked.slice());
+    }
     console.log(this.ordersChecked);
+    console.log(this.ordersChanged$);
   }
 
-  deleteOrdersChecked(ordersChecked: []) {
-    const checked = this.ordersChecked;    
+  deleteOrdersChecked(ordersChecked: Product[]) {
+    for (this.i = 0; this.i < this.ordersChecked.length; this.i++) {
+      if (this.products.find(x => x == this.ordersChecked[this.i])) {
+        this.products.splice(this.products.indexOf(this.ordersChecked[this.i]), 1);
+      }
+    }
     
-    // this.ordersChecked = this.products.filter(
-    //   product => (product.id !== ordersChecked.id)
-    // );
-
-    console.log(checked.length);
   }
 
-  deleteOrder(index: number) {
-    let orders = this.products;  
-    this.ordersChanged.next(orders.splice(index, 1));
+  deleteOrder(product: Product) {
+    let orders = this.products;
+    let index = orders.indexOf(product);
+    this.ordersChanged$.next(orders.splice(index, 1));
     console.log(index);
   }
 
