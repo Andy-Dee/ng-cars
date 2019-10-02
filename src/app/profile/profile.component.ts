9730825526cdbe183f7f5e57bd421b39c39ad038
Andriy Danilyuk <andy.danilyuk@gmail.com>
2019-10-02 13:05:19 +0300

    Auth SignUp, Fom Errors handling, Loading spinner

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  editMode: false;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    let profileName: '';
+    let profileSurname: '';
+    let profileEmail: '';
+    let profilePhone: '';
+
+    this.profileForm = new FormGroup({
+      profileName: new FormControl(profileName, Validators.required),
+      profileSurname: new FormControl(profileSurname, Validators.required),
+      profileEmail: new FormControl(profileEmail, Validators.required),
+      profilePhone: new FormControl(profilePhone, Validators.required)
+    });
   }
 
 }
