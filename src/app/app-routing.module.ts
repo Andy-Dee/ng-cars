b431ee9542a5ef76b18a8d93a38f3860e6401500
Andriy Danilyuk <andy.danilyuk@gmail.com>
2019-10-04 11:20:04 +0300

    Auth, Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,18 +8,48 @@ import { BillsComponent } from './bills/bills.component';
 import { SaleComponent } from './sale/sale.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/auth', pathMatch: 'full'},
   {path: 'auth', component: AuthComponent},
-  {path: 'orders', component: OrdersComponent},
-  {path: 'requests', component: RequestsComponent},
-  {path: 'balance', component: BalanceComponent},
-  {path: 'cars', component: CarsComponent},
-  {path: 'bills', component: BillsComponent},
-  {path: 'sale', component: SaleComponent},
-  {path: 'profile', component: ProfileComponent}
+  {
+    path: 'orders', 
+    component: OrdersComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'requests', 
+    component: RequestsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'balance', 
+    component: BalanceComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'cars', 
+    component: CarsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'bills', 
+    component: BillsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'sale', 
+    component: SaleComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile', 
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {path: '**', redirectTo: '/auth'}
 ];
 
 @NgModule({
